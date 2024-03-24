a1670283ca5cfcc5c1c75beef608c4ae863fc00f
Abhinav Pandey <79640544+pixelcaliber@users.noreply.github.com>
2024-03-24 17:46:03 +0530

    Delete logger_utils.py

--- logger_utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-# logger configs
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
